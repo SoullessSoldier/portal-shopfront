9c5d70b626a886c7a080b7d44bcba687efd1156a
SoullessSoldier <gevork_ssh@mail.ru>
2021-08-26 00:16:54 +0200

    add swiper slider, font-awesome 4.07 icon, sketches

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'ups':'Источник бесперебойного питания',
         'pc':'Системный блок',
         'phone':'Телефон',
+        'tcd':'Тонкий клиент',
     };
     const peripheralsItemList = document.querySelectorAll('.peripherals-item');
 
@@ -21,4 +22,26 @@ document.addEventListener('DOMContentLoaded', () => {
             alert("Вы выбрали: " + peripheralName);
         });
     });
+
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
 });
